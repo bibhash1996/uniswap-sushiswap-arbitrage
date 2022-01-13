c02176647cca7365dc67118e70a150ae83f86847
Bibhash Singh <bibhash1996@gmail.com>
2022-01-13 23:00:56 +0530

    kovan testnet addresses

--- contracts/Arbitrage.sol
+++ contracts/Arbitrage.sol
@@ -1,77 +1,83 @@
 pragma solidity ^0.6.6;
 
-import './UniswapV2Library.sol';
-import './interfaces/IUniswapV2Router02.sol';
-import './interfaces/IUniswapV2Pair.sol';
-import './interfaces/IUniswapV2Factory.sol';
-import './interfaces/IERC20.sol';
+import "./UniswapV2Library.sol";
+import "./interfaces/IUniswapV2Router02.sol";
+import "./interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IUniswapV2Factory.sol";
+import "./interfaces/IERC20.sol";
+
+// DAI KOVAN Address 0x9aA23C7d6be1052F1C23B39c100eDd98C538c392
+// USDT KOVAN Address 0xf3e0d7bf58c5d455d31ef1c2d5375904df525105
 
 contract Arbitrage {
-  address public factory;
-  uint constant deadline = 10 days;
-  IUniswapV2Router02 public sushiRouter;
+    address public factory;
+    uint256 constant deadline = 10 days;
+    IUniswapV2Router02 public sushiRouter;
 
-  constructor(address _factory, address _sushiRouter) public {
-    factory = _factory;  
-    sushiRouter = IUniswapV2Router02(_sushiRouter);
-  }
+    constructor(address _factory, address _sushiRouter) public {
+        factory = _factory;
+        sushiRouter = IUniswapV2Router02(_sushiRouter);
+    }
 
-  function startArbitrage(
-    address token0, 
-    address token1, 
-    uint amount0, 
-    uint amount1
-  ) external {
-    address pairAddress = IUniswapV2Factory(factory).getPair(token0, token1);
-    require(pairAddress != address(0), 'This pool does not exist');
-    IUniswapV2Pair(pairAddress).swap(
-      amount0, 
-      amount1, 
-      address(this), 
-      bytes('not empty')
-    );
-  }
+    function startArbitrage(
+        address token0,
+        address token1,
+        uint256 amount0,
+        uint256 amount1
+    ) external {
+        address pairAddress = IUniswapV2Factory(factory).getPair(
+            token0,
+            token1
+        );
+        require(pairAddress != address(0), "This pool does not exist");
+        IUniswapV2Pair(pairAddress).swap(
+            amount0,
+            amount1,
+            address(this),
+            bytes("not empty")
+        );
+    }
 
-  function uniswapV2Call(
-    address _sender, 
-    uint _amount0, 
-    uint _amount1, 
-    bytes calldata _data
-  ) external {
-    address[] memory path = new address[](2);
-    uint amountToken = _amount0 == 0 ? _amount1 : _amount0;
-    
-    address token0 = IUniswapV2Pair(msg.sender).token0();
-    address token1 = IUniswapV2Pair(msg.sender).token1();
+    function uniswapV2Call(
+        address _sender,
+        uint256 _amount0,
+        uint256 _amount1,
+        bytes calldata _data
+    ) external {
+        address[] memory path = new address[](2);
+        uint256 amountToken = _amount0 == 0 ? _amount1 : _amount0;
 
-    require(
-      msg.sender == UniswapV2Library.pairFor(factory, token0, token1), 
-      'Unauthorized'
-    ); 
-    require(_amount0 == 0 || _amount1 == 0);
+        address token0 = IUniswapV2Pair(msg.sender).token0();
+        address token1 = IUniswapV2Pair(msg.sender).token1();
 
-    path[0] = _amount0 == 0 ? token1 : token0;
-    path[1] = _amount0 == 0 ? token0 : token1;
+        require(
+            msg.sender == UniswapV2Library.pairFor(factory, token0, token1),
+            "Unauthorized"
+        );
+        require(_amount0 == 0 || _amount1 == 0);
 
-    IERC20 token = IERC20(_amount0 == 0 ? token1 : token0);
-    
-    token.approve(address(sushiRouter), amountToken);
+        path[0] = _amount0 == 0 ? token1 : token0;
+        path[1] = _amount0 == 0 ? token0 : token1;
 
-    uint amountRequired = UniswapV2Library.getAmountsIn(
-      factory, 
-      amountToken, 
-      path
-    )[0];
-    uint amountReceived = sushiRouter.swapExactTokensForTokens(
-      amountToken, 
-      amountRequired, 
-      path, 
-      msg.sender, 
-      deadline
-    )[1];
+        IERC20 token = IERC20(_amount0 == 0 ? token1 : token0);
 
-    IERC20 otherToken = IERC20(_amount0 == 0 ? token0 : token1);
-    otherToken.transfer(msg.sender, amountRequired);
-    otherToken.transfer(tx.origin, amountReceived - amountRequired);
-  }
+        token.approve(address(sushiRouter), amountToken);
+
+        uint256 amountRequired = UniswapV2Library.getAmountsIn(
+            factory,
+            amountToken,
+            path
+        )[0];
+        uint256 amountReceived = sushiRouter.swapExactTokensForTokens(
+            amountToken,
+            amountRequired,
+            path,
+            msg.sender,
+            deadline
+        )[1];
+
+        IERC20 otherToken = IERC20(_amount0 == 0 ? token0 : token1);
+        otherToken.transfer(msg.sender, amountRequired);
+        otherToken.transfer(tx.origin, amountReceived - amountRequired);
+    }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,6 +4,7 @@ module.exports = function (deployer) {
   deployer.deploy(
     Arbitrage,
     '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f', //Uniswap factory
-    '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F', //Sushiswap router
+    // '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F', //Sushiswap router
+    '0xc35DADB65012eC5796536bD9864eD8773aBc74C4' // Sushiswap kovan router
   );
 };
